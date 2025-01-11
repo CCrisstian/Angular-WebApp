4fac5cc09fc424fcceebc3d16cc6b10da3037b8c
CCrisstian <crisstiann.c@gmail.com>
2025-01-11 17:50:08 -0300

    Proyecto Angula terminado y conectado con el BackEnd del proyecto "Java_SpringBoot_API-RESTful"

--- src/app/components/products.component.ts
+++ src/app/components/products.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../models/product';
+import { EventDispatcher } from '@angular/core/primitives/event-dispatch';
 
 @Component({
   selector: 'table-product',
   imports: [],
   templateUrl: './products.component.html',
 })
+
 export class ProductsComponent {
   title = 'Listado de Productos';
+
   @Input() products: Product[] = [];
+
+  @Output() updateProductEvent = new EventEmitter();
+
+  @Output() removeProductEvent = new EventEmitter();
+  
+  onUpdateProduct(product: Product): void {
+    this.updateProductEvent.emit(product);
+  }
+
+  onRemoveProduct(id: number): void{
+    this.removeProductEvent.emit(id);
+  }
 }
